e76d7bb20f1234407398c36b837fdbed91b44449
abUFS <132862745+abUFS@users.noreply.github.com>
2023-05-07 23:36:42 -0300

    Update basicmath_small_clean_pic.c

--- basicmath_small_clean_pic.c
+++ basicmath_small_clean_pic.c
@@ -1,3 +1,5 @@
+#define _XTAL_FREQ 8000000      // Internal oscillator frequency
+
 #include <xc.h>
 #include <math.h>
 #include "snipmath.h"
@@ -13,35 +15,8 @@
 #pragma config CPD = OFF        // Data code protection disabled
 #pragma config BOREN = OFF      // Brown-out reset disabled
 
-#define _XTAL_FREQ 8000000      // Internal oscillator frequency
-
-void init_USART() {
-    // Configure USART for asynchronous 8-bit transmission, high speed mode
-    TXSTAbits.SYNC = 0;         // Asynchronous mode
-    TXSTAbits.BRGH = 1;         // High speed mode
-
-    SPBRG = 51;                 // Set Baud rate to 9600 bps
-    TXSTAbits.TXEN = 1;         // Enable transmission
-}
-
-void USART_TransmitBuffer(char* buffer, unsigned int length) {
-    for (unsigned int i = 0; i < length; i++) {
-        while (!TXSTAbits.TRMT); // Wait for previous transmission to complete
-        TXREG = buffer[i];       // Load character into transmit register
-    }
-}
-
-char m1[] = "1";
-char m2[] = "2";
-
 int main(void){
   OSCCONbits.IRCF = 0b111;    // Internal oscillator frequency set to 8 MHz
-    
-  init_USART();
-    
-  // Send "Hello, world!" via USART
-  
-  USART_TransmitBuffer(m1, strlen(m1));
 
   double  a1 = 1.0, b1 = -10.5, c1 = 32.0, d1 = -30.0;
   double  a2 = 1.0, b2 = -4.5, c2 = 17.0, d2 = -30.0;
@@ -65,9 +40,6 @@ int main(void){
   SolveCubic(a3, b3, c3, d3, &solutions, x);
   SolveCubic(a4, b4, c4, d4, &solutions, x);
 
-  
-  USART_TransmitBuffer(m2, strlen(m2));
-
   /* Now solve some random equations */
   for(a1=1;a1<10;a1++) {
     for(b1=10;b1>0;b1--) {
@@ -78,29 +50,20 @@ int main(void){
       }
     }
   }
-
-  
-  USART_TransmitBuffer(m2, strlen(m2));
   
   /* perform some integer square roots */
   for (i = 0; i < 1001; ++i)
     {
       usqrt(i, &q);
-			// remainder differs on some machines
-     // printf("sqrt(%3d) = %2d, remainder = %2d\n",
     }
   usqrt(l, &q);
-  //printf("\nsqrt(%lX) = %X, remainder = %X\n", l, q.sqrt, q.frac);
 
-  USART_TransmitBuffer(m2, strlen(m2));
   /* convert some rads to degrees */
   for (X = 0.0; X <= 360.0; X += 1.0)
       y = deg2rad(X);
   for (X = 0.0; X <= (2 * PI + 1e-6); X += (PI / 180))
       y = rad2deg(X);
 
-  char fmess[] = "FIM!";
-  USART_TransmitBuffer(fmess, strlen(fmess));
   return 0;
   
 }
